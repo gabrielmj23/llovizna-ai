6dd12505041d4a75f01a8ad915c9f38c0de5bc1e
gabrielmj23 <gabrielmj2312@gmail.com>
2025-07-10 21:55:58 -0400

    modelos ucab

--- fine-tuning/split_dataset.py
+++ fine-tuning/split_dataset.py
@@ -10,6 +10,31 @@ if len(sys.argv) < 2:
 input_folder = sys.argv[1]
 output_folder = input_folder  # El output será igual al input
 
+
+def rename_files_and_dirs(root_dir):
+    # Renombra archivos y carpetas con "+" o espacios en el nombre
+    for root, dirs, files in os.walk(root_dir, topdown=False):
+        # Archivos
+        for fname in files:
+            if "+" in fname or " " in fname:
+                new_fname = fname.replace("+", "_").replace(" ", "_")
+                old_path = os.path.join(root, fname)
+                new_path = os.path.join(root, new_fname)
+                print(f"Renombrando archivo: {old_path} -> {new_path}")
+                os.rename(old_path, new_path)
+        # Carpetas
+        for dname in dirs:
+            if "+" in dname or " " in dname:
+                new_dname = dname.replace("+", "_").replace(" ", "_")
+                old_dir = os.path.join(root, dname)
+                new_dir = os.path.join(root, new_dname)
+                print(f"Renombrando carpeta: {old_dir} -> {new_dir}")
+                os.rename(old_dir, new_dir)
+
+
+# Renombra archivos y carpetas antes de dividir
+rename_files_and_dirs(input_folder)
+
 # Porcentajes para la división: (train, validation, test)
 split_ratios = (0.8, 0.1, 0.1)
 
